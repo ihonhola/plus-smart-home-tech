362f562cf501a5ecaff1fe8601d3927b9fef2019
Anatoly Yakovlev <ihonhola@yandex.ru>
2026-05-24 12:57:10 +0300

    feat: Realization of service Config-server

--- telemetry/aggregator/src/main/java/ru/yandex/practicum/aggregator/config/KafkaProperties.java
+++ telemetry/aggregator/src/main/java/ru/yandex/practicum/aggregator/config/KafkaProperties.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Component;
 
 @Getter @Setter
 @Component
-@ConfigurationProperties(prefix = "aggregator.kafka")
+@ConfigurationProperties("aggregator.kafka")
 public class KafkaProperties {
     private Consumer consumer = new Consumer();
     private Producer producer = new Producer();
--- telemetry/analyzer/src/main/java/ru/yandex/practicum/analyzer/config/KafkaConsumerProperties.java
+++ telemetry/analyzer/src/main/java/ru/yandex/practicum/analyzer/config/KafkaConsumerProperties.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Component;
 
 @Getter
 @Setter
-@ConfigurationProperties(prefix = "analyzer.kafka")
+@ConfigurationProperties("analyzer.kafka")
 @Component
 public class KafkaConsumerProperties {
     private ConsumerConfig snapshotConsumer = new ConsumerConfig();
